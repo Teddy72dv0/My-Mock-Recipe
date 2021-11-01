9b134a3b3e6aa5e89da3a1a240708f213a021f08
Ted <jarod_ph2001@yahoo.com>
2021-11-02 05:42:38 +0800

    This is the beginning of the commit.

---
+++ My Mock Recipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Mock Recipe
+//
+//  Created by Ted Antipolo on 10/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
